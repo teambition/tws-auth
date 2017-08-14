d7b8e226ccb612c9ea7f30e59109c232a58326fd
David Cai <davidcai1993@yahoo.com>
2017-08-14 13:45:03 +0800

    fix: use JSON.stringify(res.body) to avoid `Error: [Object object]`

--- test/service/user.js
+++ test/service/user.js
@@ -1,4 +1,36 @@
 'use strict'
 const tman = require('tman')
+const assert = require('power-assert')
+const MemoryStore = require('../../lib/cache/memory')
+const User = require('../../lib/service/user')
 
-tman.suite('service - user', function () {})
+tman.suite('service - user', function () {
+  this.timeout(5000)
+
+  let user = new User({
+    cacheStore: new MemoryStore(),
+    host: 'https://121.196.214.67:31090',
+    appId: '59294da476d70b4b83fa91a5',
+    appSecret: process.env.APP_SECRET,
+    timeout: 30000
+  })
+
+  tman.it('verifyCookie - invalid parameters', function * () {
+    try {
+      yield user.verifyCookie('68e9721d-d823-d973-0d21-c14d7c29d213')
+    } catch (error) {
+      return assert(error.message, 'Invalid Parameters')
+    }
+
+    throw new Error('not throw')
+  })
+
+  tman.it('verifyToken - invalid parameters', function * () {
+    try {
+      yield user.verifyToken('invalid-token')
+    } catch (error) {
+      console.log(error)
+      // return assert()
+    }
+  })
+})
