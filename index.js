fab04205650a02930a6f7994bd572902d95c461b
zensh <admin@zensh.com>
2017-09-29 19:34:01 +0800

    improve struct

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 'use strict'
-const validator = require('validator')
+
 const Store = require('./lib/cache/store')
-const services = require('./lib/service')
+const Auth = require('./lib/service')
 
-class Client {
+class Client extends Auth {
   constructor (options) {
-    if (!validator.isMongoId(options.appId)) {
-      throw new Error(`appId: ${options.appId} is not a valid mongo id`)
-    }
-    if (options.cacheStore && !(options.cacheStore instanceof Store)) {
-      throw new TypeError('cacheStore should be an instance of Store')
-    }
+    super(options)
 
-    options.host = options.host || 'https://auth.teambitionapis.com'
-    options.timeout = options.timeout || 2000
-
-    // Services provided by TWS authorization service
-    this.auth = new services.Auth(options)
-    this.user = new services.User(options)
+    this.auth = this // compatibility
+    this.user = this.withObject(Auth.user)
   }
 }
+
+Client.Auth = Auth
+Client.user = Auth.user
+Client.UA = require('./lib/util/ua')
+
+Client.Store = Store
+Client.RedisStore = require('./lib/cache/redis')
+Client.MemoryStore = require('./lib/cache/memory')
+
+const { assertRes, assertResultWithError } = require('./lib/util/request')
+Client.assertRes = assertRes
+Client.assertResultWithError = assertResultWithError
 
 module.exports = Client
---
+++ test/index.js
@@ -0,0 +1,98 @@
+'use strict'
+
+const { suite, it } = require('tman')
+const assert = require('power-assert')
+
+const Auth = require('..')
+const MemoryStore = Auth.MemoryStore
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+suite('tws-auth', function () {
+  this.timeout(5000)
+
+  const client = new Auth({
+    cacheStore: new MemoryStore(),
+    host: 'https://121.196.214.67:31090',
+    appId: '59294da476d70b4b83fa91a5',
+    appSecret: process.env.APP_SECRET,
+    timeout: 30000
+  })
+
+  suite('service - auth', function () {
+    it('authorize by type: self', function * () {
+      let token = yield client.authorize('59294da476d70b4b83fa91a5', 'self')
+      assert(token.length !== 0)
+    })
+
+    it('authorize by type: user', function * () {
+      let token = yield client.authorize('59294da476d70b4b83fa91a0', 'user')
+      assert(token.length !== 0)
+    })
+  })
+
+  suite('service - user', function () {
+    it('authorize by type: self', function * () {
+      let token = yield client.user.authorize('59294da476d70b4b83fa91a5', 'self')
+      assert(token.length !== 0)
+    })
+
+    it('authorize by type: user', function * () {
+      let token = yield client.user.authorize('59294da476d70b4b83fa91a0', 'user')
+      assert(token.length !== 0)
+    })
+
+    it('verifyCookie - invalid parameters', function * () {
+      try {
+        yield client.user.verifyCookie('68e9721d-d823-d973-0d21-c14d7c29d213')
+      } catch ({ message }) {
+        return assert(message, 'Invalid Parameters')
+      }
+
+      throw new Error('not throw')
+    })
+
+    it('verifyToken - Unauthorized', function * () {
+      try {
+        yield client.user.verifyToken('invalid-token')
+      } catch ({ error }) {
+        return assert(error.error, 'Unauthorized')
+      }
+
+      throw new Error('not throw')
+    })
+
+    it('getById - Resource Not Found', function * () {
+      try {
+        yield client.user.getById('5109f1e918e6fcfc560001a6')
+      } catch ({ message }) {
+        return assert(message, 'Resource Not Found')
+      }
+
+      throw new Error('not throw')
+    })
+
+    it('getByEmail - Resource Not Found', function * () {
+      try {
+        yield client.user.getByEmail('test-not-found@email.email')
+      } catch ({ message }) {
+        return assert(message, 'Resource Not Found')
+      }
+
+      throw new Error('not throw')
+    })
+
+    it('batchGetbyIds', function * () {
+      try {
+        yield client.user.batchGetbyIds([
+          '5109f1e918e6fcfc560001a6',
+          '5109f1e918e6fcfc560001a7'
+        ])
+      } catch ({ error }) {
+        return assert(error.error, 'Not Found')
+      }
+
+      throw new Error('not throw')
+    })
+  })
+})
